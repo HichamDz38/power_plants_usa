86f7cb0f363917e3487a9d40eb9d9670076ba798
hichamhugintech <hicham@hugintech.com>
2021-12-02 05:20:41 +0100

    apply default migrations

--- power_plants_usa/visualizer/models.py
+++ power_plants_usa/visualizer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Plant(models.Model):
+    name 
+    facility_code
+    state_abrv
+    latitude
+    longitude
+
+class Energy(models.Model):
+    Plant_id
+    year
+    generator_id
+
