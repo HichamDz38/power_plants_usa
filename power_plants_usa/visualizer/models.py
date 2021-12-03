6c1b508a4adad8f465b0fa9acf5c4c7ef2765319
hichamhugintech <hicham@hugintech.com>
2021-12-03 04:12:04 +0100

    create the model

--- power_plants_usa/visualizer/models.py
+++ power_plants_usa/visualizer/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Plant(models.Model):
-    name 
-    facility_code
-    state_abrv
-    latitude
-    longitude
+    name = models.CharField(max_length=30)
+    facility_code = models.IntegerField()
+    state = models.CharField(max_length=3)
+    latitude = models.DecimalField()
+    longitude  = models.DecimalField()
+
+
+class Plant_information(models.Model):
+    plant_id = models.IntegerField()
+    primary_fuel = models.CharField(max_length=5)
+    num_generator = models.IntegerField()
+    nameplate_capacity = models.DecimalField()
+    num_boilers = models.IntegerField()
+    year = models.IntegerField()
 
 class Energy(models.Model):
-    Plant_id
-    year
-    generator_id
+    Plant_id = models.IntegerField()
+    generator_id =models.IntegerField()
+    unit_id = models.IntegerField()
+    year = models.IntegerField()
 
