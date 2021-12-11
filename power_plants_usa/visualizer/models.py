44599d8dd651014d471d1cfc238cdf1d57c45483
hichamhugintech <hicham@hugintech.com>
2021-12-11 23:49:24 +0100

    fix models, some data are empty in the excel file

--- power_plants_usa/visualizer/models.py
+++ power_plants_usa/visualizer/models.py
@@ -2,24 +2,24 @@ from django.db import models
 
 
 class Plant(models.Model):
-    name = models.CharField(max_length=30)
-    facility_code = models.IntegerField()
-    state = models.CharField(max_length=3)
-    latitude = models.DecimalField()
-    longitude  = models.DecimalField()
+    name = models.CharField(max_length=60)
+    facility_code = models.IntegerField(unique=True)
+    state = models.CharField(max_length=5)
+    latitude = models.DecimalField(default=None, blank=True, null=True, max_digits=10, decimal_places=6)
+    longitude  = models.DecimalField(default=None, blank=True, null=True, max_digits=10, decimal_places=6)
 
 
 class Plant_information(models.Model):
-    plant_id = models.IntegerField()
-    primary_fuel = models.CharField(max_length=5)
-    num_generator = models.IntegerField()
-    nameplate_capacity = models.DecimalField()
-    num_boilers = models.IntegerField()
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    primary_fuel = models.CharField(default=None, blank=True, null=True, max_length=5)
+    num_generator = models.IntegerField(default=None, blank=True, null=True)
+    nameplate_capacity = models.DecimalField(default=None, blank=True, null=True, max_digits=15, decimal_places=3)
+    num_boilers = models.IntegerField(default=None, blank=True, null=True)
     year = models.IntegerField()
 
 class Energy(models.Model):
-    Plant_id = models.IntegerField()
-    generator_id =models.IntegerField()
-    unit_id = models.IntegerField()
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    generator_id = models.CharField(max_length=10)
+    generator_anual_net = models.DecimalField(default=None, blank=True, null=True, max_digits=15, decimal_places=3)
     year = models.IntegerField()
 
