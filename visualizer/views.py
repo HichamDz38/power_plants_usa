9d1d779b6029a035aadabf9da7130c644fdaf840
hichamhugintech <hicham@hugintech.com>
2021-12-18 18:23:38 +0100

    add plan_information & energy views

--- visualizer/views.py
+++ visualizer/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from .scripts.import_data import Excel_import
 from django import forms
-from . import models as model
+from visualizer import models as model
 from .forms import add_data_Form
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
-from .serializers import Plant_Serializer
+from .serializers import Plant_Serializer,Plant_information_Serializer,Energy_Serializer
 
 
-def handle_uploaded_file(f):
-    Excel_import_processor = Excel_import(f, 2016)
+def handle_uploaded_file(f, year):
+    Excel_import_processor = Excel_import(f, year)
     return Excel_import_processor.process_import(model)
 
 
@@ -24,7 +24,7 @@ def import_data(request, *args, **kwargs):
     if request.POST:
         form = add_data_Form(request.POST, request.FILES)
         if form.is_valid():
-            status = handle_uploaded_file(request.FILES["data_file"])
+            status = handle_uploaded_file(request.FILES["data_file"],request.POST['year'])
             context['status'] = status
     else:
         form = add_data_Form()
@@ -35,5 +35,19 @@ class PlantAPIView(viewsets.ModelViewSet):
     """
     API endpoint for Plant
     """
-    queryset = model.Energy.objects.select_related('Plant')
+    queryset = model.Plant.objects.all().order_by('id')
     serializer_class = Plant_Serializer
+
+class Plant_informationAPIView(viewsets.ModelViewSet):
+    """
+    API endpoint for Plant
+    """
+    queryset = model.Plant_information.objects.all().order_by('id')
+    serializer_class = Plant_information_Serializer
+
+class EnergyAPIView(viewsets.ModelViewSet):
+    """
+    API endpoint for Plant
+    """
+    queryset = model.Energy.objects.prefetch_related('plant').order_by('id')
+    serializer_class = Energy_Serializer
