11341d7f2a21241aeafbd5167c517b6760a33eee
hichamhugintech <hicham@hugintech.com>
2021-12-18 18:24:56 +0100

    add routes for new views

--- power_plants_usa/urls.py
+++ power_plants_usa/urls.py
@@ -14,21 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from visualizer.views import dashboard, import_data
-from django.conf.urls import include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from visualizer import views
 
 router = DefaultRouter()
-router.register('plant_list', views.PlantAPIView)
-
+router.register(r'plant_list', views.PlantAPIView)
+router.register(r'plant_information', views.Plant_informationAPIView)
+router.register(r'energy', views.EnergyAPIView)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dachboard/', dashboard),
-    path('add_data/', import_data),
-    path('', include(router.urls)),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
-    
+    path('add_data/', views.import_data, name='import_data'),
+    path('api/', include(router.urls)),
 ]
