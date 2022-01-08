1651eb8cc12293ca16ad77ed114e4f880c260a54
HichamDz38 <dachirhicham@gmail.com>
2022-01-08 01:33:58 +0100

    install react-leaflet

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,5 +22,35 @@ function App() {
     </div>
   );
 }
+
+export default App;
+*/
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+//import MarkerClusterer from "@googlemaps/markerclustererplus";
+
+
+const App = () => (
+    <div>
+      <div>
+        <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={false} style={{ height: '90vh', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      </div>
+      <div>
+        <select>
+        </select>
+      </div>
+    </div>
+);
+
 
 export default App;
